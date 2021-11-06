9b270260f7c404a0beb246a0ca3c8b66a0097e02
skonry <skonry1604@gmail.com>
2021-11-07 00:47:48 +0100

    Added index action to ProductsController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def index
+    result = GetProductsService.call(params[:filtering], params[:pagination])
+
+    if result && result.success?
+      render json: ProductBlueprint.render(result.payload), status: :ok
+    else
+      render json: {errors: result.errors}, status: :bad_request
+  end
 end
