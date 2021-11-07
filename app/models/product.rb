44354c8b2111f5e677c3b9d461aef34e24c6be78
skonry <skonry1604@gmail.com>
2021-11-07 17:39:06 +0100

    Modyfied Product's filter_by_price to be inclusive range

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  scope :filter_by_price, ->(from, to) { where("price > ? AND price < ?", from, to) }
+  scope :filter_by_price, ->(from, to) { where("price >= ? AND price <= ?", from, to) }
 end
