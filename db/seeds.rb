188ef43237ea2bf2a85de564bc9b89ec38424571
skonry <skonry1604@gmail.com>
2021-11-07 02:23:31 +0100

    Added to seed file generations of products and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do
+  Category.create!(
+    name: Faker::Commerce.department
+  )
+end
+
+200.times do
+  p = Product.create!(
+    category_id: Faker::Number.between(from: 1, to: 20),
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 0..1000.0, as_string: true)
+  )
+  puts Faker::Commerce.price
+end
