88639f5f578be424ea48937f7677a35fedef598a
skonry <skonry1604@gmail.com>
2021-11-07 14:21:42 +0100

    Added complete and set_cart methods in CartsController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  before_action :set_cart
+
   def create
     cart = Cart.create
 
@@ -9,5 +11,15 @@ class CartsController < ApplicationController
     cart = Cart.find(params[:id])
 
     render :json CartBlueprint(cart).render, status: :ok
+  end
+
+  def complete
+    @cart.destroy
+
+    render :json {message: "Shopping complited"}, status: :ok
+  end
+
+  private def set_cart
+    @cart = Cart.find(params[:cart_id])
   end
 end
