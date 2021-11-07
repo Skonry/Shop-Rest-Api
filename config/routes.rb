564e3e965de57ed861d585cf1174b1c4ecc5e253
skonry <skonry1604@gmail.com>
2021-11-07 14:27:06 +0100

    Modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :carts do
-    resources :cart_products
+  resources :carts, only: [:create, :show] do
+    resources :cart_products, only: [:create, destroy]
   end
-  resources :products
+  get "cart/:id/complete", to "carts#complete"
+
+  resources :products, only: [:index]
+  get "products/find", to: "products#find"
 end
