5855beb39f4fb14a755385bd7eaa5f885c724072
skonry <skonry1604@gmail.com>
2021-11-07 03:33:55 +0100

    Nested in routing cart_products resoruce inside carts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :cart_products
-  resources :carts
+  resources :carts do
+    resources :cart_products
+  end
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
