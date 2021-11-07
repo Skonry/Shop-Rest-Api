d512de9c1a9be91f162ac6a2c5a606e20af4b027
skonry <skonry1604@gmail.com>
2021-11-07 03:54:31 +0100

    Added create and destroy actions to CartProductsController

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,2 +1,24 @@
 class CartProductsController < ApplicationController
+  before_action :set_cart
+
+  def create
+    cart_product = @cart.cart_products.create(cart_product_params)
+
+    render :json cart_product, status: :created
+  end
+
+  def destroy
+    cart_product = @cart.cart_product.find(params[:id])
+    cart_product.destroy
+
+    head :no_content
+  end
+
+  def private set_cart
+    @cart = Cart.find(params[:cart_id])
+  end
+
+  def private cart_product_params
+    params.permit(:card_id, :product_id)
+  end
 end
