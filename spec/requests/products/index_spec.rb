c015141c2f83ce863112d798dcb5f34a1d3f7c62
skonry <skonry1604@gmail.com>
2021-11-07 17:54:35 +0100

    Correctet errors in request specs

--- spec/requests/products/index_spec.rb
+++ spec/requests/products/index_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Products Index Action", type: :request do
-  let(:categories) { 
-    FactoryBot.create_list(:category, 5) do |category, i|
-      category.name = "Category #{i + 1}"
+  let!(:categories) { 
+    5.times do |i|
+      FactoryBot.create(:category, name: "Category #{i + 1}")
     end
   }
 
-  let(:products) {
-    FactoryBot.create_list(:product, 50) do |product, i|
-      product.name = "Product #{i + 1}"
-      product.price = i + 1
-      product.category_id = (i % 5) + 1
+  let!(:products) {
+    50.times do |i|
+      FactoryBot.create(
+        :product,
+        name: "Product #{i + 1}",
+        price: i + 1,
+        category_id: (i % 5) + 1
+      )
     end
   }
 
@@ -27,40 +30,42 @@ RSpec.describe "Products Index Action", type: :request do
     end
 
     it "returns first page of items" do
-      expect(JSON.parse(response.body).first.id).to eq(1)
+      expect(JSON.parse(response.body)[0]["id"]).to eq(1)
     end
   end
 
   context "when send valid request with page parameter" do
-    before { get "products", :params => {:pagination => {:page => 2} } }
+    before { get "/products", :params => {:pagination => {:page => 2} } }
 
     it "returns status code 200" do
       expect(response).to have_http_status(200)
     end
 
     it "returns second page of items" do
-      expect(JSON.parse(response.body).first.id).to eq(26)
+      expect(JSON.parse(response.body)[0]["id"]).to eq(26)
     end
   end
 
   context "when send valid request with category filter parameter" do
-    before { get "products", :params => {:filtering => {:category => "Category 1"} } }
+    let(:category_name) { "Category 1" }
+
+    before { get "/products", :params => {:filtering => {:category => category_name} }  }
 
     it "returns status code 200" do
       expect(response).to have_http_status(200)
     end
 
-    it "returns 3 items" do
-      expect(JSON.parse(response.body).size).to eq(3)
+    it "returns 10 items" do
+      expect(JSON.parse(response.body).size).to eq(10)
     end
 
-    it "returns 3 items that belongs to parameter category" do
-      expect(JSON.parse(response.body).first.category.id).to eq(1)
+    it "returns 10 items that belongs to parameter category" do
+      expect(JSON.parse(response.body)[0]["category"]).to eq(category_name)
     end
   end
 
   context "when send valid request with price filter parameter" do
-    before { get "products", :params => {:filtering => {:price => {:from => 5, :to => 10} } } }
+    before { get "/products", :params => {:filtering => {:price => {:from => 5, :to => 10} } } }
 
     it "returns status code 200" do
       expect(response).to have_http_status(200)
@@ -72,62 +77,40 @@ RSpec.describe "Products Index Action", type: :request do
   end
 
   context "when send invalid request with page parameter" do
-    before { get "products", :params => {:pagination => {:page => 22} } }
+    before { get "/products", :params => {:pagination => {:page => 22} } }
 
     it "returns status code 400" do
       expect(response).to have_http_status(400)
     end
 
     it "returns response with error" do
-      expect(JSON.parse(response.body).errors.size).to eq(1)
+      expect(JSON.parse(response.body)["errors"].size).to eq(1)
     end
   end
 
-  context "when send invalid request without filter parameter" do
-    before { get "products", :params => {:filtering => nil } }
-
-    it "returns status code 400" do
-      expect(response).to have_http_status(400)
-    end
-
-    it "returns response with error" do
-      expect(JSON.parse(response.body).errors.size).to eq(1)
-    end
-  end
+  
 
   context "when send invalid request with empty category filter parameter" do
-    before { get "products", :params => {:filtering => {:category => nil } } }
+    before { get "/products", :params => {:filtering => {:category => nil } } }
 
     it "returns status code 400" do
       expect(response).to have_http_status(400)
     end
 
     it "returns response with error" do
-      expect(JSON.parse(response.body).errors.size).to eq(1)
-    end
-  end
-
-  context "when send invalid request with empty price filter parameter" do
-    before { get "products", :params => {:filtering => {:price => nil } } }
-
-    it "returns status code 400" do
-      expect(response).to have_http_status(400)
-    end
-
-    it "returns response with error" do
-      expect(JSON.parse(response.body).errors.size).to eq(1)
+      expect(JSON.parse(response.body)["errors"].size).to eq(1)
     end
   end
 
   context "when send invalid request price filter without both from and to parameters" do
-    before { get "products", :params => {:filtering => {:price => {:from => nil , to => nil} } } }
+    before { get "/products", :params => {:filtering => {:price => {:from => nil , :to => nil} } } }
 
     it "returns status code 400" do
       expect(response).to have_http_status(400)
     end
 
     it "returns response with error" do
-      expect(JSON.parse(response.body).errors.size).to eq(1)
+      expect(JSON.parse(response.body)["errors"].size).to eq(1)
     end
   end
 end
--- spec/requests/products/show_spec.rb
+++ spec/requests/products/show_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Products Index Action", type: :request do
-  let(:categories) { 
-    FactoryBot.create_list(:category, 1) do |category, i|
-      category.name = "Category #{i + 1}"
+  let!(:categories) { 
+    1.times do |i|
+      FactoryBot.create(:category, name: "Category #{i + 1}")
     end
   }
 
-  let(:products) {
-    FactoryBot.create_list(:product, 10) do |product, i|
-      product.name = "Product #{i + 1}"
-      product.price = i + 1
-      product.category_id = i
+  let!(:products) {
+    10.times do |i|
+      FactoryBot.create(
+        :product,
+        name: "Product #{i + 1}",
+        price: i + 1,
+        category_id: 1
+      )
     end
   }
 
@@ -25,7 +28,7 @@ RSpec.describe "Products Index Action", type: :request do
     end
 
     it "returns product named Product 1" do
-      expect(JSON.parse(response.body).name.to eq(product_name)
+      expect(JSON.parse(response.body)["name"]).to eq(product_name)
     end
   end
 
