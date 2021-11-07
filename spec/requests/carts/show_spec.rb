8768c28f24fbdd3eb74361d283207f16a8b21a87
skonry <skonry1604@gmail.com>
2021-11-07 23:14:13 +0100

    Added test that check total price of cart

--- spec/requests/carts/show_spec.rb
+++ spec/requests/carts/show_spec.rb
@@ -22,4 +22,20 @@ RSpec.describe "Carts Show Action", type: :request do
       expect(response).to have_http_status(404)
     end
   end
+
+  context "when added products to cart" do
+    let(:cart) { Cart.create() }
+    let(:category) { FactoryBot.create(:category, name: "Category 1") }
+
+    it "returns total price of cart" do
+      
+      cart.products << FactoryBot.create(:product, name: "Product 1", category_id: category.id, price: 10.0)
+      cart.products << FactoryBot.create(:product, name: "Product 2", category_id: category.id, price: 20.0)
+      cart.products << FactoryBot.create(:product, name: "Product 3", category_id: category.id, price: 30.0)
+
+      get "/carts/#{cart.id}"
+
+      expect(JSON.parse(response.body)["total_price"]).to eq("60.0")
+    end
+  end
 end
