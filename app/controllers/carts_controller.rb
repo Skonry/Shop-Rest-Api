0d1173bd8906b66b80bac8a9f1ca52c08be13119
skonry <skonry1604@gmail.com>
2021-11-07 03:22:53 +0100

    Added create and show actions to CartController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,13 @@
 class CartsController < ApplicationController
+  def create
+    cart = Cart.create
+
+    render :json CartBlueprint(cart).render, status: :created 
+  end
+
+  def show
+    cart = Cart.find(params[:id])
+
+    render :json CartBlueprint(cart).render, status: :ok
+  end
 end
