cb58089b4f95996c05bd3d00a7607d1f3b737e7b
skonry <skonry1604@gmail.com>
2021-11-07 04:03:52 +0100

    Added total price field to Cart serializer

--- app/serializers/cart_blueprint.rb
+++ app/serializers/cart_blueprint.rb
@@ -1,5 +1,9 @@
 class CartBlueprint < Blueprinter::Base
   field :id
   
+  field :total_price do |cart|
+    cart.products.map({|product| product.price}).sum
+  end
+
   association :products, blueprint: ProductBlueprint
 end
