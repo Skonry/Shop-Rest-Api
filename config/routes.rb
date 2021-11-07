88587e301ef44110b9ece2eb6e7c471ccd07bf6f
skonry <skonry1604@gmail.com>
2021-11-07 20:34:10 +0100

    Fixed route path in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :carts, only: [:create, :show] do
     resources :cart_products, only: [:create, :destroy]
   end
-  post "cart/:id/complete", to: "carts#complete"
+  post "carts/:id/complete", to: "carts#complete"
 
   resources :products, only: [:index]
   get "products/find", to: "products#find"
