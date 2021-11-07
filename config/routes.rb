821e4c705af1504987fc5e6b5635cb7a82bbcfc9
skonry <skonry1604@gmail.com>
2021-11-07 03:14:48 +0100

    Generated Cart and CartProduct resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :cart_products
+  resources :carts
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
